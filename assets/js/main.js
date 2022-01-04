6542c28653526b4d3dea27c2d315858193dcdd60
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2022-01-03 23:42:08 -0300

    Only missing services_img be firstColor instead of black

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ const showMenu = (toggleId, navId) => {
   // Validate that variables exist
   if (toggle && nav) {
     toggle.addEventListener("click", () => {
-      // We add the show-menu class to the div tag with the nav__menu class
+      // We add the show-menu class to the div tag with the nav_menu class
       nav.classList.toggle("show-menu");
     });
   }
@@ -18,7 +18,7 @@ const navLink = document.querySelectorAll(".nav_link");
 
 function linkAction() {
   const navMenu = document.getElementById("nav-menu");
-  // When we click on each nav__link, we remove the show-menu class
+  // When we click on each nav_link, we remove the show-menu class
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
@@ -66,5 +66,57 @@ function scrollTop() {
 window.addEventListener("scroll", scrollTop);
 
 /*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 2000,
+  reset: true
+});
+
+sr.reveal(
+  `.home_data, .home_img,
+          .about_data, .about_img,
+          .services_content, .menu_content,
+          .app_data, .app_img,
+          .contact_data, .contact_button,
+          .footer_content`,
+  {
+    interval: 200
+  }
+);
